c6223c602d0f723b601c99af645299d1e8058063
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-01-27 13:11:42 +0100

    Some bit of important topics like Fragments were included

--- src/components/Columns.jsx
+++ src/components/Columns.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 const Columns = () => {
+  const items = [];
+
   return (
-    <>
+    <React.Fragment>
+      {items.map((item) => (
+        <React.Fragment key={item.id}>
+          <h1>Title</h1>
+          <p>{item.title} </p>
+        </React.Fragment>
+      ))}
       <td>Name</td>
       <td>Vishwas</td>
       <td>Row</td>
-    </>
+    </React.Fragment>
   );
 };
 
---
+++ src/components/PureComponents.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+export class PureComponents extends PureComponent {
+  render() {
+    return <div>PureComponents</div>;
+  }
+}
+
+export default PureComponents;
